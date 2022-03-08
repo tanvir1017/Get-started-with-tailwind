360bfc13efaae0b02b7ec12570a36e403dfc1b6b
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-03-09 01:53:07 +0600

    done

--- js/index.js
+++
@@ -1,8 +0,0 @@
-let depositButton = document.getElementById("deposit-button");
-let withdrawButton = document.getElementById("withdraw-button");
-
-withdrawButton.addEventListener("click", () => {
-  const withdrawMoney = document.getElementById("withdraw-money");
-  const withdrawField = document.getElementById("withdraw-field");
-  console.log(withdrawField.value);
-});
